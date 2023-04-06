1705f169f1f74ea0eb04b7facf057ee060dd4126
Stefano Pigozzi <me@steffo.eu>
2023-04-07 00:14:27 +0200

    Add `FLASK_SECRET_KEY` config parameter

--- temple_of_styx/config/__init__.py
+++ temple_of_styx/config/__init__.py
@@ -10,6 +10,13 @@ def DATABASE_URL(value) -> s.URL:
     return s.make_url(value)
 
 
+@config.required()
+def FLASK_SECRET_KEY(value) -> bytes:
+    # Generate with `os.urandom`!
+    return bytes(value, encoding="utf8")
+
+
 __all__ = (
     "DATABASE_URL",
+    "FLASK_SECRET_KEY",
 )
