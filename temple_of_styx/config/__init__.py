853190d84b96f08f8d20b09498524e0cf2c43309
Stefano Pigozzi <me@steffo.eu>
2023-04-08 01:25:40 +0200

    Add configurable `STYX_TITLE` variable

--- temple_of_styx/config/__init__.py
+++ temple_of_styx/config/__init__.py
@@ -16,7 +16,13 @@ def FLASK_SECRET_KEY(value) -> bytes:
     return bytes(value, encoding="utf8")
 
 
+@config.required()
+def STYX_TITLE(value) -> str:
+    return value
+
+
 __all__ = (
     "DATABASE_URL",
     "FLASK_SECRET_KEY",
+    "STYX_TITLE",
 )
--- temple_of_styx/web/app.py
+++ temple_of_styx/web/app.py
@@ -1,6 +1,6 @@
 import flask as f
 
-from temple_of_styx.config import DATABASE_URL, FLASK_SECRET_KEY
+from temple_of_styx.config import DATABASE_URL, FLASK_SECRET_KEY, STYX_TITLE
 from .decorators import json
 from .extensions import ext_sqla, ext_auth
 from .blueprints import health
@@ -10,6 +10,7 @@ app = f.Flask(__name__)
 
 app.config["SECRET_KEY"] = FLASK_SECRET_KEY.__wrapped__
 app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL.__wrapped__
+app.config["STYX_TITLE"] = STYX_TITLE.__wrapped__
 
 ext_sqla.init_app(app)
 ext_auth.init_app(app)
