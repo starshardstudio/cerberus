bdb21f263e296e660fc6dd8f4f76366c5ea28f86
Stefano Pigozzi <me@steffo.eu>
2023-12-24 01:05:38 +0100

    Update to Bluelib 9.0.1

--- temple_of_styx/config/__init__.py
+++ temple_of_styx/config/__init__.py
@@ -30,19 +30,9 @@ def STYX_BLUELIB_COLORS(value) -> str:
     return value
 
 
-@config.required()
-def STYX_BACKGROUND_SRC(value) -> str:
-    """
-    The background image to display on the page.
-    For example, `https://bluelib.steffo.eu/examples/fabrice-nerfin-puHQJZd3MDg-unsplash.jpg`.
-    """
-    return value
-
-
 __all__ = (
     "DATABASE_URL",
     "FLASK_SECRET_KEY",
     "STYX_TITLE",
     "STYX_BLUELIB_COLORS",
-    "STYX_BACKGROUND_SRC",
 )
