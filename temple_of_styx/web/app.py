9a2181bad7e21f7fe5222a26d789f9f6923e06f5
Stefano Pigozzi <me@steffo.eu>
2023-12-24 01:08:24 +0100

    Remove unavailable import

--- temple_of_styx/web/app.py
+++ temple_of_styx/web/app.py
@@ -1,7 +1,7 @@
 import flask as f
 import pkg_resources
 
-from temple_of_styx.config import DATABASE_URL, FLASK_SECRET_KEY, STYX_BLUELIB_COLORS, STYX_BACKGROUND_SRC, STYX_TITLE
+from temple_of_styx.config import DATABASE_URL, FLASK_SECRET_KEY, STYX_BLUELIB_COLORS, STYX_TITLE
 from .extensions import ext_sqla, ext_login, ext_auth
 from .blueprints import health, login
 
@@ -11,7 +11,6 @@ app = f.Flask(__name__)
 app.config["SECRET_KEY"] = FLASK_SECRET_KEY.__wrapped__
 app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL.__wrapped__
 app.config["STYX_BLUELIB_COLORS"] = STYX_BLUELIB_COLORS.__wrapped__
-app.config["STYX_BACKGROUND_SRC"] = STYX_BACKGROUND_SRC.__wrapped__
 app.config["STYX_TITLE"] = STYX_TITLE.__wrapped__
 
 ext_sqla.init_app(app)
